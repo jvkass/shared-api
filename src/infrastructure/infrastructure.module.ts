0386ea40ebf387905a4daf45205c592a598ea8fa
jvkass <jvcnmata@gmail.com>
2023-02-26 11:38:20 -0300

    workflow to publish package

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,13 +1,11 @@
 import { Global, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { ValidateJwtGuardModule } from "./adapters/auth/jwt-auth-guard/validate-jwt-guard.module";
 import { RedisCacheModule } from "./adapters/cache/redis/redis-cache.module";
 import { HealthCheckModule } from "./adapters/health-check/health-check.module";
 
 const adapters = [
   ConfigModule,
   HealthCheckModule,
-  ValidateJwtGuardModule,
   RedisCacheModule,
 ];
 
